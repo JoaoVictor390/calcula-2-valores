2a8a6bbe51819bc4e6e8a987c305e6885b84f36e
lorimbh <victoron436@gmail.com>
2025-02-09 17:26:55 -0300

    primeiro commit

---
+++ script1.js
@@ -0,0 +1,6 @@
+let num1 = Number(prompt("qual primeiro digito"))
+let num2 = Number(prompt("qual segundo digito"))
+
+resultado = num1 + num2
+console.log("resultado e; " + resultado)
+alert("O resultado e: " + resultado)
